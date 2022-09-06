1c2d8fba615a774fd556ad8ef80045f6c938b16a
viktor.lostak <viktor.lostak@avirtu.net>
2022-09-06 15:37:33 +0200

    Minor improvements in site detail skeleton.

--- src/routes/[campaignId=uuid]/site/[siteId=uuid]/+page.svelte
+++ src/routes/[campaignId=uuid]/site/[siteId=uuid]/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Site detail</h1>
+<script>
+	import { page } from '$app/stores';
+
+	let campaignId = $page.params.campaignId;
+	let siteId = $page.params.siteId;
+	let selectedView;
+</script>
+
+<h1>Site {siteId} detail</h1>
--- src/routes/site-detail.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Site detail</h1>
